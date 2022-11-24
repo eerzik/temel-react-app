783bcf1591bd695e7a5eca47cdd8631d9a35b360
Engin ERZİK <erzik.engin@gmail.com>
2022-11-24 14:33:49 +0300

    UseState

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,25 @@
+
+import { useState } from "react";
+
 const Home = () => {
 
+    const [sayac,setSayac]=useState(0);
+ 
+    const handleClick=()=>{
+        
+        setSayac(3);
+    }
 
     return (
         <div>
             <h2>AnaSayfa</h2>
-            <button onClick={(e) => { console.log(e); }}>Tıkla</button>
+            <p>{sayac}</p>
+            <button onClick={handleClick}>Tıkla</button>
         </div>
     );
 
 
 
-    // const handleClick = () => {
-    //     console.log("merhaba")
-    // }
-
-    // const handleClick2 = (isim,e) => {
-    //     console.log(isim,e);
-    // }
-
-    // return (
-    //     <h2>AnaSayfa</h2>
-    //         {/* parametre kullanmadığımız için parantez aç kapa yapmıyoruz eğer parantez açarsak sayfa yenilendiğinde console merhaba yazar */}
-    //         {/* <button onClick={handleClick()}>Tıkla</button></> */}
-    //         <button onClick={(e) => handleClick2('luffy',e)}>Tıkla</button></>
-    // );
 }
 
 export default Home;
