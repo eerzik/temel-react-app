3f760518967ac049b22b2c2bc70a4308c0486f5c
Engin ERZİK <erzik.engin@gmail.com>
2022-11-24 14:25:47 +0300

    Click Eventi

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,29 @@
 const Home = () => {
-    return ( 
-        <h2>AnaSayfa</h2>
-     );
+
+
+    return (
+        <div>
+            <h2>AnaSayfa</h2>
+            <button onClick={(e) => { console.log(e); }}>Tıkla</button>
+        </div>
+    );
+
+
+
+    // const handleClick = () => {
+    //     console.log("merhaba")
+    // }
+
+    // const handleClick2 = (isim,e) => {
+    //     console.log(isim,e);
+    // }
+
+    // return (
+    //     <h2>AnaSayfa</h2>
+    //         {/* parametre kullanmadığımız için parantez aç kapa yapmıyoruz eğer parantez açarsak sayfa yenilendiğinde console merhaba yazar */}
+    //         {/* <button onClick={handleClick()}>Tıkla</button></> */}
+    //         <button onClick={(e) => handleClick2('luffy',e)}>Tıkla</button></>
+    // );
 }
- 
+
 export default Home;
