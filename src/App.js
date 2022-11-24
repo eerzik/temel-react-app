4022b6eb703a51061319625ebf4b6dc27d342a72
Engin ERZİK <erzik.engin@gmail.com>
2022-11-24 10:51:11 +0300

    CssStyle ve Component örnekleri

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import Navbar from "./Navbar";
+import AnaSayfa from "./Home";
 
 
 function App() {
 
   const baslik = 'AOS Blog'
+  let okunmaSayisi = 300
+  const durum = true
+  const blog = { ad: "ilk", aciklama: "ilk açıklama" };
+  const link = "www.google.com"
+
+
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div className="content">
-        <h1>{baslik}</h1>
+        <AnaSayfa></AnaSayfa>
+        
+        <p>----------------------</p>
 
+        <h1>{baslik}</h1>
+        <h1>{"Başlık yerine kullandım"}</h1>
+        <p>Okunma Sayısı {okunmaSayisi}</p>
+        <p>Durum {durum}</p>
+        <p>Blog Ad {blog.ad}</p>
+        <p>Blog Açıklama {blog.aciklama}</p>
+        <a href={link}>Google</a>
       </div>
 
     </div>
---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>AOS Blog</h1>
+            <div className="links">
+                <a href="/">Ana Sayfa</a>
+                <a href="/create" style={{
+                    color:'white',
+                    backgroundColor:'#ff793f',
+                    borderRadius:'8px'
+                }}>Yeni Yazı</a>
+
+            </div>
+        </nav>
+
+    );
+}
+
+export default Navbar;
