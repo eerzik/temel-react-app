9668e55d005a20909926e309f7391ee59c4b7d17
Engin ERZİK <erzik.engin@gmail.com>
2022-11-25 13:54:08 +0300

    Verilere erişme işlemi

--- src/Home.js
+++ src/Home.js
@@ -3,44 +3,18 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            ad: 'Yeni başlayanlar için c#',
-            aciklama: "lorem ipsum",
-            yazar: 'luffy',
-            id: 1
-        },
+    const [blogs, setBlogs] = useState(null)
 
-        {
-            ad: 'React öğreniyorum',
-            aciklama: "lorem ipsum",
-            yazar: 'zoro',
-            id: 2
-        },
-
-        {
-            ad: 'İleri seviye c#',
-            aciklama: "lorem ipsum",
-            yazar: 'luffy',
-            id: 3
-        },
-
-        {
-            ad: 'MVC Öğreniyorum',
-            aciklama: "lorem ipsum",
-            yazar: 'sanji',
-            id: 4
-        }
-    ])
-
-    const [isim, setIsim] = useState('luffy');
-
-    // [] eklemezsek usestate lerde de çalışır eklersek sadece sayfa yüklenirken bir kez çalışır.
-    //[blogs] olarak yazarsak bloglar ile ilgili değişim olursa çalışır.
     useEffect(() => {
-        console.log("useEffect çalıştı");
-
-    }, [blogs, isim])
+        fetch('http://localhost:8000/yazilar')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                // console.log(data);
+                setBlogs(data)
+            })
+    }, [])
 
     const handleClick = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -49,8 +23,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            <BlogList bloglar={blogs} baslik="Bütün Yazılar" handleClick={handleClick} />
-            <button onClick={()=>setIsim('zoro')}>Değiştir</button>
+            {blogs && <BlogList bloglar={blogs} baslik="Bütün Yazılar" handleClick={handleClick} />}
         </div>
 
     );
