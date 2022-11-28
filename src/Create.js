c85308cb0b96922516e56f280e9f89b570216cb7
Engin ERZİK <erzik.engin@gmail.com>
2022-11-28 17:20:42 +0300

    Create için useState oluşturma

--- src/Create.js
+++ src/Create.js
@@ -1,23 +1,35 @@
+
+import { useState } from "react";
+
 const Create = () => {
+
+    const [baslik, setBaslik] = useState('');
+    const [aciklama, setAciklama] = useState('');
+    const [yazar, setYazar] = useState('Luffy');
+
+
+
     return (
         <div className="create">
             <h2 style={{ color: '#ff793f' }}>Yeni Yazı Ekle</h2>
             <form>
                 <label>Yazı Başlık :
-                    <input type="text" required></input>
+                    <input type="text" required value={baslik} onChange={(e) => setBaslik(e.target.value)}></input>
                 </label>
                 <label>Yazı Açıklama :   </label>
-                <textarea  required></textarea>
+                <textarea required value={aciklama} onChange={(e) => setAciklama(e.target.value)}></textarea>
 
                 <label>Yazar: </label>
-                <select>
+                <select value={yazar} onChange={(e) => setYazar(e.target.value)}>
                     <option value="Luffy">Luffy</option>
                     <option value="Zoro">Zoro</option>
                     <option value="Sanji">Sanji</option>
                 </select>
 
                 <button>Ekle</button>
-
+                {/* {baslik}
+                {yazar}
+                {aciklama} */}
             </form>
         </div>
     );
