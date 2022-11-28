dbc9fedfabe103dcd60a399895c5dff6086b3a76
Engin ERZİK <erzik.engin@gmail.com>
2022-11-28 15:56:00 +0300

    Router Oluşturma

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import Navbar from "./Navbar";
 import AnaSayfa from "./Home";
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
 
-  const baslik = 'AOS Blog'
-  let okunmaSayisi = 300
-  const durum = true
-  const blog = { ad: "ilk", aciklama: "ilk açıklama" };
-  const link = "www.google.com"
-
-
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content">
-        <AnaSayfa></AnaSayfa>
-        
-   
-        {/* <h1>{baslik}</h1>
-        <h1>{"Başlık yerine kullandım"}</h1>
-        <p>Okunma Sayısı {okunmaSayisi}</p>
-        <p>Durum {durum}</p>
-        <p>Blog Ad {blog.ad}</p>
-        <p>Blog Açıklama {blog.aciklama}</p>
-        <a href={link}>Google</a> */}
-      </div>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<AnaSayfa/>}>
+              
+            </Route>
+          </Routes>
 
-    </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
