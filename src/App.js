d80b31edf2e92be75a4784a86bc8098698adfa1b
Engin ERZİK <erzik.engin@gmail.com>
2022-11-29 16:21:43 +0300

    Hata Sayfası Oluşturma

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import AnaSayfa from "./Home";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+
+import NotFound from "./NotFound";
+
 function App() {
 
   return (
@@ -13,11 +16,9 @@ function App() {
           <Routes>
             <Route path="/" element={<AnaSayfa />}></Route>
             <Route path="/create" element={<Create />}></Route>
-
             <Route path="/blogs/:id" element={<BlogDetails />}></Route>
-
+            <Route path="*" element={<NotFound/>}></Route>
           </Routes>
-
         </div>
       </div>
     </Router>
